6658fe2984373a450bd286aed08774186d7998d1
Akiyoshi <zundokokiyoshi999@gmail.com>
2021-04-11 06:52:46 +0900

    Torello_app Topページ作成

---
+++ 02_Trello-style-app/my-trello/src/components/Board.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <header>my Trello</header>
+    <main>
+        <p class="info-line">All: 0 task</p>
+    </main>
+</div>
+    
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+</script>
